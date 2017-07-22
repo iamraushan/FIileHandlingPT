7c5f257bbd30fe6a38ca3be2826da32f747878a0
iamraushan <raushan.sh@live.com>
2017-07-22 15:31:45 +0530

    Practc

--- src/Log/Log.java
+++ src/Log/Log.java
@@ -16,19 +16,19 @@ public class Log {
 	    		
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    		String filename="Default.txt";
-	    		String workdir =System.getProperty("user.dir");
-	    		File workingDirFile = new File(workdir); 
-	    		File testfile = new File(workingDirFile, filename);
-	    		if(testfile.exists())
+	    		String workdir =System.getProperty("user.dir");//getting the working directory
+	    		File workingDirFile = new File(workdir);//making file object with workdir property
+	    		File testfile = new File(workingDirFile, filename);//making file object with workingdirfile
+	    		if(testfile.exists())//checking whether that file exists or not
 	    		{
 	    			System.out.println("Log exists");
 	    			String myLine=null;
-	    			FileReader input = new FileReader("Default.txt");
-					BufferedReader bufRead = new BufferedReader(input);
-	    			while ( (myLine = bufRead.readLine()) != null) 
+	    			FileReader input = new FileReader("Default.txt"); //Name of the file -- this reads characters 
+					BufferedReader bufRead = new BufferedReader(input);//
+	    			while ( (myLine = bufRead.readLine()) != null) //reads lines at once
 					{    
 					    //System.out.println(myLine);
-						String[] arr = myLine.split("\\s+");
+						String[] arr = myLine.split("\\s+");//divides stuffs wrt spaces
 						if(arr[1].toString().equals(pw))
 						{
 							System.out.println("LOGIN SUCCESSFULL");
@@ -47,11 +47,11 @@ public class Log {
 	    			System.out.println("Log do not exists");
 	    			System.out.println("Using default log");
 	    			File f = new File("Default.txt");
-	    			if(f.createNewFile())
+	    			if(f.createNewFile())//creating new file
 	    			{
 	    				System.out.println("Log created");
 	    				
-						PrintWriter out = new PrintWriter(f);
+						PrintWriter out = new PrintWriter(f);//in order to write text in txt file
 	    				
 	    				
 	    				if(uname.equals("user") && pw.equals("1234"))
@@ -63,8 +63,8 @@ public class Log {
 	    						FileReader input = new FileReader("Default.txt");
 	    						BufferedReader bufRead = new BufferedReader(input);
 	    						String myLine ="user "+npw;
-	    						out.println(myLine);
-    							out.close();
+	    						out.println(myLine);//writing to file
+    							out.close();//needed
     							System.out.println("Password Changed");
 	    					
 	    					
